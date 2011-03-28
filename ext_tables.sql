13286051de7591368aea9053431ec085af19408d
fab1en <fab1en@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-28 09:08:05 +0000

    [BUGFIX] change wrong field type
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/taxonomy/trunk@45768 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -174,7 +174,7 @@ CREATE TABLE tx_taxonomy_domain_model_note (
 	
 	concept int(11) unsigned DEFAULT '0' NOT NULL,
 	
-	tx_taxonomy_type int(11) DEFAULT '0' NOT NULL,
+	tx_taxonomy_type varchar(255) DEFAULT '' NOT NULL,
 	tx_taxonomy_value text NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
